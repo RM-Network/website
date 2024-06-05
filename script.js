b7ce50ae67261d7c898b0a1f8306976f1398d835
Noah Rider <media@noahrider.com>
2024-06-05 00:10:30 -0400

    wait page

---
+++ script.js
@@ -0,0 +1,4 @@
+// get current year for footer
+var currentYear = new Date().getFullYear();
+var yearElement = document.getElementById("year");
+yearElement.innerHTML = "© 2023-" + currentYear + " RM Network";
